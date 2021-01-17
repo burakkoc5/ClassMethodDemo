fb3f294294b0baefe9014399dea8650ee5775563
Burak Koç <burak.koc@tedu.edu.tr>
2021-01-17 15:54:54 +0300

    Add project files.

---
+++ ClassMethodDemo/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class Customer
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int id { get; set; }
+
+
+    }
+}
+
